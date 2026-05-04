095c24a96cc283dc5ea6cf08c1cc081cb513ee87
thiago <thiagocarv4@gmail.com>
2026-05-04 11:01:15 -0300

    feat: handler para lidar com erros nos controllers

---
+++ server/desafio_estagio/src/main/java/com/thiago/desafio_estagio/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,12 @@
+package com.thiago.desafio_estagio.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+
+  private String field;
+  private String message;
+}
