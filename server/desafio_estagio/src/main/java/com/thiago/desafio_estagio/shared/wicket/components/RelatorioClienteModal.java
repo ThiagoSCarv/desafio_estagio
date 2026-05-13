5b8eb5915b0a2c66ffbf7440f2ebe6349b88dd0b
thiago <thiagocarv4@gmail.com>
2026-05-13 10:40:52 -0300

    feat: modal para gerar relatorio

---
+++ server/desafio_estagio/src/main/java/com/thiago/desafio_estagio/shared/wicket/components/RelatorioClienteModal.java
@@ -0,0 +1,42 @@
+package com.thiago.desafio_estagio.shared.wicket.components;
+
+import com.thiago.desafio_estagio.cliente.application.ClienteDto;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.panel.Panel;
+
+import java.util.UUID;
+
+public class RelatorioClienteModal extends Panel {
+
+    private UUID clienteId;
+
+    public RelatorioClienteModal(String id) {
+        super(id);
+        setOutputMarkupId(true);
+
+        add(new AjaxLink<Void>("gerarPdf") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                target.appendJavaScript(
+                    "window.open('/relatorio/clientes/" + clienteId + "?formato=pdf', '_blank');" +
+                    "bootstrap.Modal.getOrCreateInstance(document.getElementById('" + RelatorioClienteModal.this.getMarkupId() + "')).hide();"
+                );
+            }
+        });
+
+        add(new AjaxLink<Void>("gerarXlsx") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                target.appendJavaScript(
+                    "window.open('/relatorio/clientes/" + clienteId + "?formato=xlsx', '_blank');" +
+                    "bootstrap.Modal.getOrCreateInstance(document.getElementById('" + RelatorioClienteModal.this.getMarkupId() + "')).hide();"
+                );
+            }
+        });
+    }
+
+    public void setCliente(ClienteDto cliente) {
+        this.clienteId = cliente.id();
+    }
+}
