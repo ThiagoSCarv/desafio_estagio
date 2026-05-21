ce47bee374033d8de0f9bc3843f5da8b657522bf
thiago <thiagocarv4@gmail.com>
2026-05-21 13:52:48 -0300

    feat: migrando ButtonComponent de mat-icon para lucide-icon

--- desafio_estagio_angular/src/app/shared/shared.module.ts
+++ desafio_estagio_angular/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { LucideAngularModule, UserPlus, FileText, Upload } from 'lucide-angular';
 
 import { ButtonComponent } from './components/button/button.component';
 
@@ -11,6 +12,7 @@ import { ButtonComponent } from './components/button/button.component';
     CommonModule,
     MatIconModule,
     MatProgressSpinnerModule,
+    LucideAngularModule.pick({ UserPlus, FileText, Upload }),
   ],
   exports: [ButtonComponent],
 })
